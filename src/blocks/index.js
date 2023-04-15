260f6e91409f5e54415c56b55e7fd019dea3aa33
Nathan <nathan.armstrong21@gmail.comgit>
2023-04-15 16:22:12 -0600

    Added tailwind, modified tawilwind config. added basic component and block structure.

---
+++ src/blocks/index.js
@@ -0,0 +1,6 @@
+export { About } from "./about";
+export { BuyNow } from "./buyNow";
+export { GetCreative } from "./getCreative";
+export { Hero } from "./hero";
+export { Reviews } from "./reviews";
+export { SellingInfo } from "./sellingInfo";
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      gray: "#f5f5f5",
+      "gray-light": "#f9f9f9",
+      "gray-dark": "#333333",
+      "gray-blue": "#d7e1e9",
+      "gray-blue-light": "#cbdeef",
+      // "gray-blue-dark": "#a8d3ef",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};

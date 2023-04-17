e52905ad8c26406137d28a99f82ba47c3fcfc4f3
Nathan <nathan.armstrong21@gmail.comgit>
2023-04-17 10:06:16 -0600

    add body text to highlights and style. added text to sellinginfo, updated styles. mobile pass

--- src/blocks/sellingInfo/index.js
+++ src/blocks/sellingInfo/index.js
@@ -10,10 +10,25 @@ export const SellingInfo = () => {
           other unwanted metals is simple!
         </h2>
         <hr className="w-16 border-gray-blue-light border my-16" />
-        <div className="grid grid-cols-3 gap-16">
-          <Highlights />
-          <Highlights />
-          <Highlights />
+        <p className="my-16">
+          Sell gold, silver, platinum, or palladium in any amount. You get a
+          quick quote and the
+          <br />
+          best price, no matter how much you sell.
+        </p>
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-32  mt-28">
+          <Highlights
+            title="Request your free label"
+            body="Fill out the form and choose your preferred method of shipping. Once we receive your request, our agent will send you an insured shipping label and instructions via email."
+          />
+          <Highlights
+            title="Ship your metal"
+            body="Track your insured shipment every step of the way via Canada Post or UPS. All shipments are fully insured."
+          />
+          <Highlights
+            title="Get paid!"
+            body="Within 24 hours after receiving the metal, one of our certified technicians will appraise your item(s) based on metal content and send you an offer. Once your email. order is confirmed, payment will be deposited directly into your bank account."
+          />
         </div>
       </Inset>
     </section>
--- src/components/highlights/index.js
+++ src/components/highlights/index.js
@@ -1,11 +1,12 @@
 export const Highlights = ({ title, body }) => {
   return (
     <div>
-      <div className="">
-        <div className="relative h-6 bg-gray-blue-light">
-          <h3 className="absolute left-2 right-2 bottom-3">title</h3>
-        </div>
+      <div className="relative h-6 bg-gray-blue-light">
+        <h3 className="absolute left-2 right-2 bottom-3 font-bold text-xl">
+          {title}
+        </h3>
       </div>
+      <p className="leading-loose mt-14">{body}</p>
     </div>
   );
 };
